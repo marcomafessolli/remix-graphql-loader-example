bd4bd5133071536f93a6eb4fb4d16c86e02a430b
Marco Antônio Mafessolli <marcomafessolli@gmail.com>
2021-12-16 19:23:55 -0300

    feat: 🎸 adds chache exchange

--- app/utils/gql.server.ts
+++ app/utils/gql.server.ts
@@ -1,25 +1,31 @@
-import { createClient } from "@urql/core";
-import type { Client as URQLClient } from "@urql/core";
+import { createClient, dedupExchange, fetchExchange } from "@urql/core";
+import type { Client } from "@urql/core";
+
+import { cacheExchange } from "@urql/exchange-graphcache";
 
 const API_URL = "https://rickandmortyapi.com/graphql/";
-let client: URQLClient;
+
+let client: Client;
+
+let clientConfig = {
+  url: API_URL,
+  exchanges: [
+    dedupExchange,
+    cacheExchange({}),
+    fetchExchange,
+  ]
+}
 
 declare global {
-  var __client: URQLClient | undefined;
+  var __client: Client | undefined;
 }
 if (process.env.NODE_ENV === "production") {
-  client = createClient({
-    url: API_URL,
-    requestPolicy: "cache-and-network",
-  });
+  client = createClient(clientConfig);
 } else {
   if (!global.__client) {
     console.log("creating client");
 
-    global.__client = createClient({
-      url: API_URL,
-      requestPolicy: "network-only",
-    });
+    global.__client = createClient(clientConfig);
   }
 
   client = global.__client;
